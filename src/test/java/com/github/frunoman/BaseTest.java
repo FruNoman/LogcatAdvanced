25c6b6a8a0443b3235d4cc05a07902969054178a
Dmytro A Frolov <dmytro.a.frolov@globallogic.com>
2019-05-06 12:11:36 +0300

    add format command

--- src/test/java/com/github/frunoman/BaseTest.java
+++ src/test/java/com/github/frunoman/BaseTest.java
@@ -6,11 +6,15 @@ import java.util.Date;
 
 public class BaseTest {
 
-    @Test
-    public void test() throws Exception {
-        Logger.Logcat logcat = new Logger.Logcat();
+    @Test(priority = 1)
+    public void dumpTest() throws Exception {
+        Logger logger = new Logger.Logcat("adb").udid("0000").build();
 
-        Date date = new Date();
-        System.out.println(logcat.setTime("05-03 09:55:35.473").dump().build());
+        logger.dump();
+        for(String log:logger.readLogs()){
+            System.out.println(log);
+        }
     }
+
+
 }
